1b998b6e96fba7b9dd5742f5551a960d04799f78
Julia Yu <rubyjoycat@gmail.com>
2024-04-29 08:38:22 -0400

    description updated (libraries), test revisions

--- tests/testthat/test-noho_forecast.R
+++ tests/testthat/test-noho_forecast.R
@@ -1,30 +1,23 @@
-test_that("correct column number", {
-  northampton_forecast_data <- weather_noho()
+northampton_forecast_data <- weather_noho()
 
+test_that("correct column number", {
   expect_equal(length(northampton_forecast_data), 11)
 })
 
 test_that("correct row number", {
-  northampton_forecast_data <- weather_noho()
-
   expect_equal(length(northampton_forecast_data[[1]]), 15)
 })
 
 test_that("correct column names", {
-  northampton_forecast_data <- weather_noho()
-
-  expect_named(northampton_forecast_data, c("Day", "Date", "Status", "High", "Low", "Precipitation
-  Type", "Precipitation Chance", "Wind Direction", "Wind Speed", "City", "State"))
+  expect_named(northampton_forecast_data, c("Day", "Date", "Status", "High", "Low",
+                                            "Precipitation Type", "Precipitation Chance",
+                                            "Wind Direction", "Wind Speed", "City", "State"))
 })
 
-test_that("highs are numeric", {
-  northampton_forecast_data <- weather_noho()
-
-  expect_type(northampton_forecast_data[["High"]], "numeric")
+test_that("highs are doubles", {
+  expect_type(northampton_forecast_data[["High"]], "double")
 })
 
-test_that("lows are numeric", {
-  northampton_forecast_data <- weather_noho()
-
-  expect_type(northampton_forecast_data[["Low"]], "numeric")
+test_that("lows are doubles", {
+  expect_type(northampton_forecast_data[["Low"]], "double")
 })
